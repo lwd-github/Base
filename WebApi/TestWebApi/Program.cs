13f56d1c0cd1dee0319d5a1b88c0d5a4633ba8ad
lwd-github <lwdgithub@123>
2021-05-05 09:41:48 +0800

    测试代码

--- WebApi/TestWebApi/Program.cs
+++ WebApi/TestWebApi/Program.cs
@@ -5,6 +5,7 @@ using Config;
 using DTO.Constant;
 using IOC;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace TestWebApi
@@ -25,6 +26,9 @@ namespace TestWebApi
                 .ConfigureServices((hostingContext, services) =>
                 {
                     ConfigAgent.Configuration = hostingContext.Configuration;
+
+                    ////注册Redis；在Startup.cs的方法public void ConfigureServices(IServiceCollection services)注册也可以
+                    //services.AddSingleton(typeof(Cache.Redis.RedisCache), new Cache.Redis.RedisCache(Config.ConfigAgent.Value<Cache.Redis.Config.RedisConfig>()));
                 })
                 //方式一：需在Startup.cs 增加方法注册接口：public void ConfigureContainer(Autofac.ContainerBuilder builder)
                 //.UseServiceProviderFactory(new AutofacServiceProviderFactory()); //指定 Autofac 工厂替换默认工厂
