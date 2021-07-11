38ec2bf8b0bb1dc23ebf1415ef1008ed4b4dca6e
lwd-github <lwdgithub@123>
2021-07-11 22:58:23 +0800

    SignalR

---
+++ SignalR/SignalRClient/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace SignalRClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            HubConnection connection = new HubConnectionBuilder()
+               .WithUrl("https://localhost:5001/msghub")
+               .Build();
+
+            connection.On<string>("Self", cid =>
+            {
+                var newMessage = $"{cid}";
+
+                Console.WriteLine($"From Service => {newMessage}");
+            });
+
+            connection.On<string, string>("ReceiveMsg", (user, message) =>
+            {
+                var newMessage = $"{user}: {message}";
+
+                Console.WriteLine($"From Service => {newMessage}");
+            });
+
+            await connection.StartAsync();
+
+            await connection.InvokeAsync("SendMsg", "jack", "hello,world");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SignalR/SignalRService/MessageHub.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRService
+{
+    public class MessageHub : Hub
+    {
+        /// <summary>
+        /// 客户连接成功时触发
+        /// </summary>
+        /// <returns></returns>
+        public override async Task OnConnectedAsync()
+        {
+            var cid = Context.ConnectionId;
+            Console.WriteLine($"[{cid}]连接成功");
+
+            //根据id获取指定客户端
+            var client = Clients.Client(cid);
+
+            //向指定用户发送消息
+            await client.SendAsync("Self", cid);
+
+            ////像所有用户发送消息
+            //await Clients.All.SendAsync("AddMsg", $"{cid}加入了聊天室");
+        }
+
+
+        /// <summary>
+        /// 客户端断开连接时触发
+        /// </summary>
+        /// <param name="exception"></param>
+        /// <returns></returns>
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            var cid = Context.ConnectionId;
+            Console.WriteLine($"[{cid}]断开连接");
+        }
+
+
+        public async Task SendMsg(string user, string message)
+        {
+            Console.WriteLine($"From Client => {user}:{message}");
+            await Clients.All.SendAsync("ReceiveMsg", user, message);
+        }
+    }
+}
