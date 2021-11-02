bee4e2f6a38f6e063f4021b10907e9debabc980f
lwd-github <lwdgithub@123>
2021-11-02 23:09:00 +0800

    文字识别

--- Test/XUnitTest/BaiduAI/CharacterRecognitionTest.cs
+++ Test/XUnitTest/BaiduAI/CharacterRecognitionTest.cs
@@ -9,19 +9,25 @@ using Framework.Common.Extension;
 using System.IO;
 using System.Net.Http.Headers;
 using Baidu.Aip.Ocr;
+using System.Web;
 
 namespace XUnitTest.BaiduAI
 {
     public class CharacterRecognitionTest
     {
+        //https://cloud.baidu.com/product/ocr
         //https://www.cnblogs.com/jerryqi/p/11592676.html
 
         [Fact]
         public void RecognitionTest()
         {
-            //Recognition();
+            Recognition();
 
             Recognition1();
+
+            Recognition2();
+
+            Recognition3();
         }
 
 
@@ -66,8 +72,6 @@ namespace XUnitTest.BaiduAI
             var accessToken = GetAccessToken();
 
             HttpClient httpClient = new HttpClient();
-            httpClient.DefaultRequestHeaders.Add("access_token", accessToken);
-            //httpClient.DefaultRequestHeaders.Add("Content-Type", "application/x-www-form-urlencoded");
 
             //MultipartFormDataContent => multipart / form-data
             //FormUrlEncodedContent => application / x-www-form-urlencoded
@@ -75,14 +79,25 @@ namespace XUnitTest.BaiduAI
             //StreamContent => binary
 
             //文字图片的base64编码后的结果
-            byte[] bytes = File.ReadAllBytes(@"C:\Users\duan\Desktop\过磅单.jpg");
+            byte[] bytes = File.ReadAllBytes(@"C:\Users\future\Desktop\过磅单图片数据-02.jpg");
             string base64 = Convert.ToBase64String(bytes);
+            var image = HttpUtility.UrlEncode(base64);
 
-            FormUrlEncodedContent content = new FormUrlEncodedContent(new Dictionary<string, string>()
+            var list = new List<KeyValuePair<string, string>>
             {
-                { "image", base64 }
+                new KeyValuePair<string, string>("access_token",accessToken),
+                new KeyValuePair<string, string>("image",image),
+                new KeyValuePair<string, string>("language_type","CHN_ENG")
+            };
+
+            var data = new List<string>();
+            list.ForEach(item =>
+            {
+                data.Add($"{item.Key}={item.Value}");
             });
-            content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/x-www-form-urlencoded");
+
+            var content = new StringContent(string.Join("&", data.ToArray()));
+
             var Result = httpClient.PostAsync(url, content).Result;
 
             if (Result.StatusCode == System.Net.HttpStatusCode.OK)
@@ -99,7 +114,7 @@ namespace XUnitTest.BaiduAI
 
 
         private void Recognition1()
-        { 
+        {
             string apiKey = "wd2RatemhCiO7M5qfq9DXoAp";
             string secretKey = "eEw8ZGc83YeTLpyXroojh8dUpvaaqC74";
 
@@ -108,14 +123,85 @@ namespace XUnitTest.BaiduAI
                 Timeout = 30000 //超时时间30秒
             };
             //文字图片的base64编码后的结果
-            byte[] bytes = File.ReadAllBytes(@"C:\Users\duan\Desktop\过磅单.jpg");
-            var result = client.GeneralBasic(bytes).ToString().ToObject<GeneralBasicResult>();
+            byte[] bytes = File.ReadAllBytes(@"C:\Users\future\Desktop\过磅单图片数据-02.jpg");
+            var jobj = client.AccurateBasic(bytes);
+            var result = client.AccurateBasic(bytes).ToString().ToObject<GeneralBasicResult>();
+        }
+
+
+        private string Recognition2()
+        {
+            string responseJson;
+            var url = "https://aip.baidubce.com/rest/2.0/solution/v1/iocr/recognise";
+
+            var accessToken = GetAccessToken();
+
+            HttpClient httpClient = new HttpClient();
+
+            //MultipartFormDataContent => multipart / form-data
+            //FormUrlEncodedContent => application / x-www-form-urlencoded
+            //StringContent => application / json等
+            //StreamContent => binary
+
+            //文字图片的base64编码后的结果
+            byte[] bytes = File.ReadAllBytes(@"C:\Users\future\Desktop\过磅单图片数据-02.jpg");
+            string base64 = Convert.ToBase64String(bytes);
+            var image = HttpUtility.UrlEncode(base64);
+
+            var list = new List<KeyValuePair<string, string>>
+            {
+                new KeyValuePair<string, string>("access_token",accessToken),
+                new KeyValuePair<string, string>("image",image),
+                new KeyValuePair<string, string>("Content-Type","application/x-www-form-urlencoded"),
+                new KeyValuePair<string, string>("templateSign","537edc3c01fdcafc5da4a4e4a6881d2a")
+            };
+
+            var data = new List<string>();
+            list.ForEach(item =>
+            {
+                data.Add($"{item.Key}={item.Value}");
+            });
+
+            var content = new StringContent(string.Join("&", data.ToArray()));
+
+            var Result = httpClient.PostAsync(url, content).Result;
+
+            if (Result.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                responseJson = Result.Content.ReadAsStringAsync().Result;
+            }
+            else
+            {
+                throw new Exception("Request Url is " + url + " Response is " + Result.StatusCode);
+            }
+
+            return responseJson;
+        }
+
+
+        private string Recognition3()
+        {
+            string apiKey = "wd2RatemhCiO7M5qfq9DXoAp";
+            string secretKey = "eEw8ZGc83YeTLpyXroojh8dUpvaaqC74";
+
+            Ocr client = new Ocr(apiKey, secretKey)
+            {
+                Timeout = 30000 //超时时间30秒
+            };
+            //文字图片的base64编码后的结果
+            byte[] bytes = File.ReadAllBytes(@"C:\Users\future\Desktop\过磅单图片数据-02.jpg");
+            var dic = new Dictionary<string, object>
+                        {
+                            { "templateSign", "537edc3c01fdcafc5da4a4e4a6881d2a" }
+                        };
+            var result = client.Custom(bytes, dic).ToString();
+            return result;
         }
     }
 
 
     public class TokenInfo
-    { 
+    {
         public string refresh_token { get; set; }
         public string expires_in { get; set; }
         public string session_key { get; set; }
@@ -131,7 +217,7 @@ namespace XUnitTest.BaiduAI
     }
 
     public class WordsResult
-    { 
+    {
         public string words { get; set; }
     }
 }
---
+++ Test/XUnitTest/Pdf/PdfTest.cs
@@ -0,0 +1,108 @@
+﻿using Framework.IdGenerator;
+using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace XUnitTest.Pdf
+{
+    public class PdfTest
+    {
+        [Fact]
+        public void FindImageInPDF()
+        {
+            //byte[] bytes = File.ReadAllBytes(@"C:\Users\future\Desktop\XK3190-DS9.pdf");
+            //byte[] bytes = File.ReadAllBytes(@"C:\Users\future\Desktop\20211015导入单据（单图片）.pdf");
+            byte[] bytes = File.ReadAllBytes(@"C:\Users\future\Desktop\20211015导入单据（多图片-8张）.pdf");
+            string base64 = Convert.ToBase64String(bytes);
+
+            RandomAccessFileOrArray randomAccessFileOrArray = new RandomAccessFileOrArray(bytes);
+            using (PdfReader pdfReader = new PdfReader(randomAccessFileOrArray, null))
+            {
+                IdWorker idWorker = new IdWorker(1, 1);
+                for (int i = 1; i <= pdfReader.NumberOfPages; i++)
+                {
+                    PdfDictionary pg = pdfReader.GetPageN(i);
+                    // recursively search pages, forms and groups for images.
+                    PdfObject pdfObject = FindImageInPDFDictionary(pg);
+
+                    if (pdfObject != null)
+                    {
+                        int XrefIndex = Convert.ToInt32(((PRIndirectReference)pdfObject).Number.ToString(System.Globalization.CultureInfo.InvariantCulture));
+                        PdfObject pdfObj = pdfReader.GetPdfObject(XrefIndex);
+                        PdfStream pdfStrem = (PdfStream)pdfObj;
+                        byte[] data = PdfReader.GetStreamBytesRaw((PRStream)pdfStrem);
+                        if ((data != null))
+                        {
+                            using (MemoryStream memStream = new MemoryStream(data))
+                            {
+                                memStream.Position = 0;
+                                Image img = Image.FromStream(memStream);
+                                if (img.Height > img.Width)
+                                {
+                                    img.RotateFlip(RotateFlipType.Rotate270FlipNone);
+                                }
+
+                                using (var ms = new MemoryStream())
+                                {
+                                    img.Save(ms, ImageFormat.Jpeg);
+
+                                    using (FileStream fs = new FileStream($"C:\\Users\\future\\Desktop\\20\\{idWorker.NextId()}.jpg", FileMode.OpenOrCreate))
+                                    {
+                                        BinaryWriter w = new BinaryWriter(fs);
+                                        w.Write(ms.ToArray());
+                                    }
+                                }
+
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private static PdfObject FindImageInPDFDictionary(PdfDictionary pg)
+        {
+            PdfDictionary res = (PdfDictionary)PdfReader.GetPdfObject(pg.Get(PdfName.RESOURCES));
+
+            if(res != null)
+            {
+                PdfDictionary xobj = (PdfDictionary)PdfReader.GetPdfObject(res.Get(PdfName.XOBJECT));
+                if (xobj != null)
+                {
+                    foreach (PdfName name in xobj.Keys)
+                    {
+                        PdfObject obj = xobj.Get(name);
+                        if (obj.IsIndirect())
+                        {
+                            PdfDictionary tg = (PdfDictionary)PdfReader.GetPdfObject(obj);
+                            PdfName type = (PdfName)PdfReader.GetPdfObject(tg.Get(PdfName.SUBTYPE));
+
+                            //image at the root of the pdf
+                            if (PdfName.IMAGE.Equals(type))
+                            {
+                                return obj;
+                            }// image inside a form
+                            else if (PdfName.FORM.Equals(type))
+                            {
+                                return FindImageInPDFDictionary(tg);
+                            } //image inside a group
+                            else if (PdfName.GROUP.Equals(type))
+                            {
+                                return FindImageInPDFDictionary(tg);
+                            }
+                        }
+                    }
+                }
+            }
+            
+            return null;
+        }
+    }
+}
